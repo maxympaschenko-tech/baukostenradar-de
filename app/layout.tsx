5ff4245a4bae78e6ac3d299b9bb496cd25e4b3c4
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-26 14:56:47 +0300

    Load premium footer styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "./portal.css";
 import "./request.css";
 import "./premium.css";
+import "./premium-footer.css";
 import { ConsentManager } from "@/components/consent-manager";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
