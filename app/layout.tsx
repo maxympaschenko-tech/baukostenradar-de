9569fcbf619e82dc84952bc9579f023d15044268
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-26 12:01:53 +0300

    Load portal styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./portal.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { siteConfig } from "@/lib/site";
