92f8c2950ff221a69473f66b5760a6b15a33e33c
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-08-31 16:41:18 +0300

    Align Ratgeber social metadata description

--- app/ratgeber/layout.tsx
+++ app/ratgeber/layout.tsx
@@ -1,7 +1,7 @@
 import { socialMetadata } from "@/lib/social-metadata";
 
 const title = "Ratgeber zu Sanierung, Renovierung & Handwerkerkosten 2026";
-const description = "Ratgeber zu Sanierungskosten, Dach, Fenster, Elektrik, Heizung, Wärmepumpen, Photovoltaik, Dämmung, Kellerabdichtung, Fassaden, Garten und Außenanlagen, Sanitär, Malerarbeiten, Fliesen, Bodenbelägen, Trockenbau, Türen, Estrich, Abriss, Entsorgung, Maurerarbeiten, Treppen, Küchenrenovierung und konkreten Projektgrößen mit Richtwerten und Beispielbudgets.";
+const description = "Ratgeber zu Sanierungskosten, Dach, Fenster, Elektrik, Heizung, Wärmepumpen, Photovoltaik, Dämmung, Kellerabdichtung, Fassaden, Garten und Außenanlagen, Sanitär, Malerarbeiten, Fliesen, Bodenbelägen, Trockenbau, Türen, Estrich, Abriss, Entsorgung, Maurerarbeiten, Treppen, Küchenrenovierung, Balkon, Garage, Carport, Schornstein, Kamin und konkreten Projektgrößen mit Richtwerten und Beispielbudgets.";
 
 export const metadata = socialMetadata({
   title,
