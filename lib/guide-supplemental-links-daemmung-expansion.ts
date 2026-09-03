0278ad7faf9fe44b9f0f17fe6ed95ee625a2a06e
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-03 12:02:04 +0300

    Link Warmdach and Kaltdach into Dämmung silo

--- lib/guide-supplemental-links-daemmung-expansion.ts
+++ lib/guide-supplemental-links-daemmung-expansion.ts
@@ -15,6 +15,8 @@ const daemmungPeers: Peer[] = [
   { slug: "zwischensparrendaemmung-kosten-pro-qm", label: "Zwischensparrendämmung Kosten", href: "/ratgeber/zwischensparrendaemmung-kosten-pro-qm" },
   { slug: "untersparrendaemmung-kosten-pro-qm", label: "Untersparrendämmung Kosten", href: "/ratgeber/untersparrendaemmung-kosten-pro-qm" },
   { slug: "aufsparrendaemmung-kosten-pro-qm", label: "Aufsparrendämmung Kosten", href: "/ratgeber/aufsparrendaemmung-kosten-pro-qm" },
+  { slug: "flachdach-warmdaemmung-kosten-pro-qm", label: "Warmdach Dämmung Kosten", href: "/ratgeber/flachdach-warmdaemmung-kosten-pro-qm" },
+  { slug: "flachdach-kaltdaemmung-kosten-pro-qm", label: "Kaltdach Dämmung Kosten", href: "/ratgeber/flachdach-kaltdaemmung-kosten-pro-qm" },
   { slug: "daemmung-oberste-geschossdecke-kosten-pro-qm", label: "Oberste Geschossdecke dämmen Kosten", href: "/ratgeber/daemmung-oberste-geschossdecke-kosten-pro-qm" },
   { slug: "einblasdaemmung-geschossdecke-kosten-pro-qm", label: "Einblasdämmung Geschossdecke Kosten", href: "/ratgeber/einblasdaemmung-geschossdecke-kosten-pro-qm" },
   { slug: "daemmung-kellerdecke-kosten-pro-qm", label: "Kellerdecke dämmen Kosten", href: "/ratgeber/daemmung-kellerdecke-kosten-pro-qm" },
