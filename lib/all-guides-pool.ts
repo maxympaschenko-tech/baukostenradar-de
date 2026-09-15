bf964adaf7de1f456b3b27c43f0eea7432dca52e
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 17:29:56 +0300

    Register GFK vs concrete pool guide

--- lib/all-guides-pool.ts
+++ lib/all-guides-pool.ts
@@ -1,10 +1,12 @@
 import { allGuides as fenceGuides } from "./all-guides-fence";
+import { gfkBetonpoolVergleichGuide } from "./guide-gfk-betonpool-vergleich";
 import { poolGuides } from "./guides-pool";
 
-const poolSlugs = new Set(poolGuides.map((guide) => guide.slug));
+const poolExpansionGuides = [...poolGuides, gfkBetonpoolVergleichGuide];
+const poolSlugs = new Set(poolExpansionGuides.map((guide) => guide.slug));
 const baseWithoutPoolGuides = fenceGuides.filter((guide) => !poolSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutPoolGuides, ...poolGuides];
+export const allGuides = [...baseWithoutPoolGuides, ...poolExpansionGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
