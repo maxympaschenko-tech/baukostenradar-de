de87fef83663e7b17462c6827a8b7c057defd406
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 19:18:10 +0300

    Register Hausanbau versus Dachaufstockung guide

--- lib/all-guides-hausanbau.ts
+++ lib/all-guides-hausanbau.ts
@@ -1,10 +1,12 @@
 import { allGuides as dachausbauGuides } from "./all-guides-dachausbau";
+import { hausanbauDachaufstockungVergleichGuide } from "./guide-hausanbau-dachaufstockung-vergleich";
 import { hausanbauGuides } from "./guides-hausanbau";
 
-const hausanbauSlugs = new Set(hausanbauGuides.map((guide) => guide.slug));
+const localHausanbauGuides = [...hausanbauGuides, hausanbauDachaufstockungVergleichGuide];
+const hausanbauSlugs = new Set(localHausanbauGuides.map((guide) => guide.slug));
 const baseWithoutHausanbauGuides = dachausbauGuides.filter((guide) => !hausanbauSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutHausanbauGuides, ...hausanbauGuides];
+export const allGuides = [...baseWithoutHausanbauGuides, ...localHausanbauGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
