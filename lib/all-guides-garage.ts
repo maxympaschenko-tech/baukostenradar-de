8397c0a5a956db4fd664218e2c720eaaeaffb5d9
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 15:49:37 +0300

    Register garage comparison guide

--- lib/all-guides-garage.ts
+++ lib/all-guides-garage.ts
@@ -1,10 +1,12 @@
 import { allGuides as balconyGuides } from "./all-guides-balcony";
+import { fertiggarageMassivgarageVergleichGuide } from "./guide-fertiggarage-massivgarage-vergleich";
 import { garageGuides } from "./guides-garage";
 
-const garageSlugs = new Set(garageGuides.map((guide) => guide.slug));
+const localGarageGuides = [...garageGuides, fertiggarageMassivgarageVergleichGuide];
+const garageSlugs = new Set(localGarageGuides.map((guide) => guide.slug));
 const baseWithoutGarageGuides = balconyGuides.filter((guide) => !garageSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutGarageGuides, ...garageGuides];
+export const allGuides = [...baseWithoutGarageGuides, ...localGarageGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
