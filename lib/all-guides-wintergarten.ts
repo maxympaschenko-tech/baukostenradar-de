d75b47512e4db3001d92f661c1fcccc4e096616a
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 17:33:14 +0300

    Register winter garden comparison guide

--- lib/all-guides-wintergarten.ts
+++ lib/all-guides-wintergarten.ts
@@ -1,10 +1,12 @@
 import { allGuides as terraceCoverGuides } from "./all-guides-terrace-cover";
+import { kaltWohnwintergartenVergleichGuide } from "./guide-kalt-wohnwintergarten-vergleich";
 import { wintergartenGuides } from "./guides-wintergarten";
 
-const wintergartenSlugs = new Set(wintergartenGuides.map((guide) => guide.slug));
+const wintergartenExpansionGuides = [...wintergartenGuides, kaltWohnwintergartenVergleichGuide];
+const wintergartenSlugs = new Set(wintergartenExpansionGuides.map((guide) => guide.slug));
 const baseWithoutWintergartenGuides = terraceCoverGuides.filter((guide) => !wintergartenSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutWintergartenGuides, ...wintergartenGuides];
+export const allGuides = [...baseWithoutWintergartenGuides, ...wintergartenExpansionGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
