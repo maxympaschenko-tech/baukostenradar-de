0d106a956518a64b749b77e5c9f18c7736ac0304
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 19:34:15 +0300

    Register Wintergarten versus Terrassenueberdachung guide

--- lib/all-guides-wintergarten.ts
+++ lib/all-guides-wintergarten.ts
@@ -1,8 +1,13 @@
 import { allGuides as terraceCoverGuides } from "./all-guides-terrace-cover";
 import { kaltWohnwintergartenVergleichGuide } from "./guide-kalt-wohnwintergarten-vergleich";
+import { wintergartenTerrassenueberdachungVergleichGuide } from "./guide-wintergarten-terrassenueberdachung-vergleich";
 import { wintergartenGuides } from "./guides-wintergarten";
 
-const wintergartenExpansionGuides = [...wintergartenGuides, kaltWohnwintergartenVergleichGuide];
+const wintergartenExpansionGuides = [
+  ...wintergartenGuides,
+  kaltWohnwintergartenVergleichGuide,
+  wintergartenTerrassenueberdachungVergleichGuide,
+];
 const wintergartenSlugs = new Set(wintergartenExpansionGuides.map((guide) => guide.slug));
 const baseWithoutWintergartenGuides = terraceCoverGuides.filter((guide) => !wintergartenSlugs.has(guide.slug));
 
