b055000d6fc37873b535f9ba84d251272a8c4175
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 18:43:58 +0300

    Register Dachgaube versus Dachfenster guide

--- lib/all-guides-dachausbau.ts
+++ lib/all-guides-dachausbau.ts
@@ -1,10 +1,15 @@
 import { allGuides as wintergartenGuides } from "./all-guides-wintergarten";
+import { dachgaubeDachfensterVergleichGuide } from "./guide-dachgaube-dachfenster-vergleich";
 import { dachausbauGuides } from "./guides-dachausbau-current";
 
-const dachausbauSlugs = new Set(dachausbauGuides.map((guide) => guide.slug));
+const localDachausbauGuides = [
+  ...dachausbauGuides,
+  dachgaubeDachfensterVergleichGuide,
+];
+const dachausbauSlugs = new Set(localDachausbauGuides.map((guide) => guide.slug));
 const baseWithoutDachausbauGuides = wintergartenGuides.filter((guide) => !dachausbauSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutDachausbauGuides, ...dachausbauGuides];
+export const allGuides = [...baseWithoutDachausbauGuides, ...localDachausbauGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
