82ce956052699ccebce82f7f236595082173d7ad
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 18:37:35 +0300

    Register Keller comparison guide

--- lib/all-guides-kellerbau.ts
+++ lib/all-guides-kellerbau.ts
@@ -1,10 +1,15 @@
 import { allGuides as hausanbauGuides } from "./all-guides-hausanbau";
+import { fertigkellerMassivkellerVergleichGuide } from "./guide-fertigkeller-massivkeller-vergleich";
 import { kellerbauGuides } from "./guides-kellerbau";
 
-const kellerbauSlugs = new Set(kellerbauGuides.map((guide) => guide.slug));
+const localKellerbauGuides = [
+  ...kellerbauGuides,
+  fertigkellerMassivkellerVergleichGuide,
+];
+const kellerbauSlugs = new Set(localKellerbauGuides.map((guide) => guide.slug));
 const baseWithoutKellerbauGuides = hausanbauGuides.filter((guide) => !kellerbauSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutKellerbauGuides, ...kellerbauGuides];
+export const allGuides = [...baseWithoutKellerbauGuides, ...localKellerbauGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
