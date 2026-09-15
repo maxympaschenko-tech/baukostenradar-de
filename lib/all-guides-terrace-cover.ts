0fc5d8a5b373fdd90815b2a774b7e7f6c81c33b4
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 16:23:47 +0300

    Register terrace roof material comparison guide

--- lib/all-guides-terrace-cover.ts
+++ lib/all-guides-terrace-cover.ts
@@ -1,10 +1,15 @@
 import { allGuides as terraceGuides } from "./all-guides-terrace";
+import { terrassendachGlasKunststoffVergleichGuide } from "./guide-terrassendach-glas-kunststoff-vergleich";
 import { terraceCoverGuides } from "./guides-terrace-cover";
 
-const terraceCoverSlugs = new Set(terraceCoverGuides.map((guide) => guide.slug));
+const terraceCoverExpansionGuides = [
+  ...terraceCoverGuides,
+  terrassendachGlasKunststoffVergleichGuide,
+];
+const terraceCoverSlugs = new Set(terraceCoverExpansionGuides.map((guide) => guide.slug));
 const baseWithoutTerraceCoverGuides = terraceGuides.filter((guide) => !terraceCoverSlugs.has(guide.slug));
 
-export const allGuides = [...baseWithoutTerraceCoverGuides, ...terraceCoverGuides];
+export const allGuides = [...baseWithoutTerraceCoverGuides, ...terraceCoverExpansionGuides];
 
 export function getAnyGuide(slug: string) {
   return allGuides.find((guide) => guide.slug === slug);
