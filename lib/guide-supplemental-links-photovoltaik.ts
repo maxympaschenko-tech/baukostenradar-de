4d0c083cb37f86786006f1ae7b5ef293872a8e85
maxympaschenko-tech <maxympaschenko@gmail.com>
2026-09-15 14:38:59 +0300

    Restore supplemental link alias architecture

--- lib/guide-supplemental-links-photovoltaik.ts
+++ lib/guide-supplemental-links-photovoltaik.ts
@@ -1,9 +1,9 @@
 import {
   withSupplementalGuideLinks as withBaseSupplementalGuideLinks,
   type GuideSupplementalLink,
-} from "./guide-supplemental-links-base";
+} from "./guide-supplemental-links";
 
-export type { GuideSupplementalLink } from "./guide-supplemental-links-base";
+export type { GuideSupplementalLink } from "./guide-supplemental-links";
 
 type PhotovoltaikPeer = GuideSupplementalLink & { slug: string };
 
